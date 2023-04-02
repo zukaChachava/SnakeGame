ba1a813530e7159c0f538d3d5f111df5f5e7e06f
zuka <zurabi.chachava.1@btu.edu.ge>
2023-04-02 23:38:37 +0400

    fixed wasm file

--- www/index.js
+++ www/index.js
@@ -1,9 +1,8 @@
 async function init(){
-    const response = await fetch('sum.wasm');
+    const response = await fetch("sum.wasm");
     const buffer = await response.arrayBuffer();
     const wasm = await WebAssembly.instantiate(buffer);
-    
-    console.log(wasm.instance.exports);
+  
     const sumFunction = wasm.instance.exports.sum;
     const result = sumFunction(10, 20);
     console.log(result);
