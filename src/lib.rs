fe42765172af507c8e0e5c95c7dbfbec0b9b210c
zuka <zurabi.chachava.1@btu.edu.ge>
2023-04-10 09:57:41 +0400

    Added snake

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,38 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-pub fn greet(name: &str){
-    println!("Hello {}", name);
+struct Snake{
+    body: Vec<Cell>
 }
+
+struct Cell(usize);
 
 #[wasm_bindgen]
 pub struct World{
-    pub width: usize
+    width: usize,
+    snake: Snake
 }
 
 #[wasm_bindgen]
 impl World{
     pub fn new() -> World{
         World{
-            width: 8
+            width: 16,
+            snake: Snake::new(12)
         }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn snake_head(&self) -> usize{
+        self.snake.body[0].0
+    }
+}
+
+impl Snake{
+    fn new(index: usize) -> Snake {
+        Snake { body: vec![Cell(index)] }
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,7 +1,31 @@
 import init, {greet, World} from 'snake_game';
 
 init().then(_ => {
-    greet("zura");
+    const cellSize = 10;
     const world = World.new();
-    console.log(world.width);
+    const worldWidth = world.width();
+
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext("2d");
+    
+    canvas.width = worldWidth * cellSize;
+    canvas.height = worldWidth * cellSize;
+
+    function drawWorld(){
+        ctx.beginPath();
+
+        for(let x = 0; x < worldWidth + 1; x++){
+            ctx.moveTo(cellSize * x, 0);
+            ctx.lineTo(cellSize * x, cellSize * worldWidth);
+        }
+
+        for(let y = 0; y < worldWidth + 1; y++){
+            ctx.moveTo(0, cellSize * y);
+            ctx.lineTo(cellSize * worldWidth, cellSize * y);
+        }
+
+        ctx.stroke();
+    }
+
+    drawWorld();
 });
