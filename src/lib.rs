b4cadf967e8ba0a591065b2cbe4c368956fc8ce8
zuka <zurabi.chachava.1@btu.edu.ge>
2023-04-15 19:44:12 +0400

    Snake directions implemented

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,25 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[wasm_bindgen]
+#[derive(PartialEq, Copy, Clone)]
+pub enum Direction{
+    Up,
+    Right,
+    Down,
+    Left
+}
+
+#[derive(PartialEq)]
+enum DirectionChange {
+    Previous(Direction),
+    Empty
+}
+
 struct Snake{
-    body: Vec<Cell>
+    body: Vec<Cell>,
+    direction: Direction,
+    direction_change: DirectionChange 
 }
 
 struct Cell(usize);
@@ -17,10 +34,10 @@ pub struct World{
 
 #[wasm_bindgen]
 impl World{
-    pub fn new() -> World{
+    pub fn new(width: usize, start_index: usize) -> World{
         World{
-            width: 8,
-            snake: Snake::new(2)
+            width: width,
+            snake: Snake::new(start_index)
         }
     }
 
@@ -29,22 +46,110 @@ impl World{
     }
 
     pub fn snake_head(&self) -> usize{
-        self.snake.body[0].0
+        self.snake.get_head()
+    }
+
+    pub fn change_direction(&mut self, direction: Direction){
+        self.snake.change_direction(direction);
     }
 
     pub fn update(&mut self){
-        let mut index = self.snake_head() + 1;
-
-        if index > self.width * self.width - 1 {
-            index = 0;
-        }
-
-        self.snake.body[0].0 = index;
+        self.snake.update(self.width());
     }
 }
 
 impl Snake{
     fn new(index: usize) -> Snake {
-        Snake { body: vec![Cell(index)] }
+        Snake { body: vec![Cell(index)], direction: Direction::Right, direction_change: DirectionChange::Empty }
+    }
+
+    fn get_head(&self) -> usize {
+        self.body[0].0
+    }
+
+    fn change_direction(&mut self, direction: Direction){
+
+        let valid_move = match &self.direction_change {
+            DirectionChange::Empty => Snake::direction_valid(&self.direction, &direction),
+            DirectionChange::Previous(from) => Snake::direction_valid(&from, &direction)
+        };
+
+        if !valid_move {
+            return;
+        }
+
+        self.direction_change = DirectionChange::Previous(self.direction);
+        self.direction = direction;
+    }
+
+    fn direction_valid(from: &Direction, direction: &Direction) -> bool {
+        if *from == Direction::Left && *direction == Direction::Right ||
+           *from == Direction::Right && *direction == Direction::Left  ||
+           *from == Direction::Up && *direction == Direction::Down ||
+           *from == Direction::Down && *direction == Direction::Up  {
+            return false;
+        }
+
+        return true;
+    }
+
+    fn update(&mut self, width: usize){
+        match self.direction {
+            Direction::Right => self.move_right(width),
+            Direction::Down => self.move_down(width),
+            Direction::Left => self.move_left(width),
+            Direction::Up => self.move_up(width)
+        }
+
+        self.direction_change = DirectionChange::Empty;
+    }
+
+    fn move_right(&mut self, width: usize){
+        let mut index = self.get_head();
+
+        if (index + 1) % width == 0  {
+            index = index - (width - 1);
+        }
+        else{
+            index += 1;
+        }
+
+        self.body[0].0 = index;
+    }
+
+    fn move_down(&mut self, width: usize){
+        let mut index = self.get_head() + width;
+
+        if index > width * width - 1 {
+            index = index % width;
+        }
+
+        self.body[0].0 = index;
+    }
+
+    fn move_left(&mut self, width: usize){
+        let mut index = self.get_head();
+
+        if index % width == 0{
+            index += width - 1;
+        }
+        else{
+            index -= 1;
+        }
+
+        self.body[0].0 = index;
+    }
+
+    fn move_up(&mut self, width: usize){
+        let mut index = self.get_head();
+
+        if index < width {
+            index = width * width - width  + index
+        }
+        else{
+            index -= width;
+        }
+
+        self.body[0].0 = index;
     }
 }
--- www/index.ts
+++ www/index.ts
@@ -1,8 +1,12 @@
-import init, {World} from 'snake_game';
+import init, {World, Direction} from 'snake_game';
 
 init().then(_ => {
     const cellSize = 40;
-    const world = World.new();
+    const width = 8;
+    const start_index = Date.now() % (width * width);
+    const speed = 1;
+
+    const world = World.new(width, start_index);
     const worldWidth = world.width();
 
     const canvas = <HTMLCanvasElement> document.getElementById('canvas');
@@ -44,8 +48,32 @@ init().then(_ => {
             drawSnake();
             world.update();
             requestAnimationFrame(update);
-        }, 500);
+        }, 500 / speed);
+    }
+
+    function change_directions(){
+        window.addEventListener('keydown', (e) => {
+           switch(e.code){
+               case 'ArrowUp':
+                   change_direction(Direction.Up);
+                   break;
+               case 'ArrowDown':
+                   change_direction(Direction.Down);
+                   break;
+               case 'ArrowLeft':
+                   change_direction(Direction.Left);
+                   break;
+               case 'ArrowRight':
+                   change_direction(Direction.Right);
+                   break;
+           }
+        });
+    }
+
+    function change_direction(direction: Direction){
+        world.change_direction(direction);
     }
 
    update();
+   change_directions();
 });
