627e82c20e1e7d1194a567a5863d6229ec9a4921
zuka <55925389+zukaChachava@users.noreply.github.com>
2023-04-20 20:20:12 +0400

    Gamne States implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,69 +1,91 @@
-use wasm_bindgen::prelude::*;
 use getrandom;
+use wasm_bindgen::prelude::*;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 #[derive(PartialEq, Copy, Clone)]
-pub enum Direction{
+pub enum Direction {
     Up,
     Right,
     Down,
-    Left
+    Left,
 }
 
-enum SnakeUpdateState{
+#[wasm_bindgen]
+#[derive(PartialEq, Copy, Clone)]
+pub enum GameStatus {
+    NotStarted,
+    Started,
+    Finished,
+    Failed,
+}
+
+enum SnakeUpdateState {
     Grown,
-    Same
+    Same,
+    Dead,
+    Won,
 }
 
 #[derive(PartialEq)]
 enum DirectionChange {
     Previous(Direction),
-    Empty
+    Empty,
 }
 
-struct Snake{
+struct Snake {
     body: Vec<Cell>,
     direction: Direction,
-    direction_change: DirectionChange 
+    direction_change: DirectionChange,
 }
 
 struct Cell(usize);
 
 #[wasm_bindgen]
-pub struct World{
+pub struct World {
     width: usize,
+    game_status: GameStatus,
     snake: Snake,
-    reward_cell: usize
+    reward_cell: usize,
 }
 
-fn get_random(modulo: usize) -> usize{
+fn get_random(modulo: usize) -> usize {
     let mut bytes: [u8; 1] = [0];
     let result = getrandom::getrandom(&mut bytes);
 
     match result {
         Result::Ok(_) => bytes[0] as usize / modulo,
-        Result::Err(_) => panic!("Random Error")
+        Result::Err(_) => panic!("Random Error"),
     }
 }
 
 #[wasm_bindgen]
-impl World{
-    pub fn new(width: usize, start_index: usize) -> World{
-        World{
+impl World {
+    pub fn new(width: usize, start_index: usize) -> World {
+        World {
             width: width,
+            game_status: GameStatus::NotStarted,
             snake: Snake::new(start_index),
-            reward_cell: get_random(width - 1) 
+            reward_cell: get_random(width - 1),
         }
+    }
+
+    pub fn start_game(&mut self) {
+        if self.game_status == GameStatus::NotStarted {
+            self.game_status = GameStatus::Started;
+            return;
+        }
+
+        panic!("Invalid Game Status");
     }
 
     pub fn width(&self) -> usize {
         self.width
     }
 
-    pub fn snake_head(&self) -> usize{
+    pub fn snake_head(&self) -> usize {
         self.snake.get_head()
     }
 
@@ -71,28 +93,46 @@ impl World{
         self.snake.len()
     }
 
-    pub fn snake_body_position(&self, body_index: usize) -> usize{
+    pub fn snake_body_position(&self, body_index: usize) -> usize {
         self.snake.get_location(body_index)
     }
 
-    pub fn change_direction(&mut self, direction: Direction){
+    pub fn change_direction(&mut self, direction: Direction) {
         self.snake.change_direction(direction);
     }
 
-    pub fn get_reward_cell(&self) -> usize{
+    pub fn get_reward_cell(&self) -> usize {
         self.reward_cell
     }
 
-    pub fn update(&mut self){
+    pub fn get_game_status(&self) -> GameStatus {
+        self.game_status
+    }
+
+    pub fn update(&mut self) {
+        if self.game_status != GameStatus::Started {
+            panic!("Can Not Update NonStarted Game");
+        }
+
         let status = self.snake.update(self.width(), self.reward_cell);
 
         match status {
             SnakeUpdateState::Grown => self.generate_reward_cell(),
-            SnakeUpdateState::Same => return
+            SnakeUpdateState::Same => return,
+            SnakeUpdateState::Dead => self.game_failed(),
+            SnakeUpdateState::Won => self.game_finished(),
         }
     }
 
-    fn generate_reward_cell(&mut self){
+    fn game_finished(&mut self) {
+        self.game_status = GameStatus::Finished
+    }
+
+    fn game_failed(&mut self) {
+        self.game_status = GameStatus::Failed
+    }
+
+    fn generate_reward_cell(&mut self) {
         let mut new_index = get_random(self.width - 1);
 
         while self.snake.index_taken(new_index) {
@@ -103,9 +143,13 @@ impl World{
     }
 }
 
-impl Snake{
+impl Snake {
     fn new(index: usize) -> Snake {
-        Snake { body: vec![Cell(index), Cell(index - 1), Cell(index - 2)], direction: Direction::Right, direction_change: DirectionChange::Empty }
+        Snake {
+            body: vec![Cell(index)],
+            direction: Direction::Right,
+            direction_change: DirectionChange::Empty,
+        }
     }
 
     fn get_head(&self) -> usize {
@@ -116,19 +160,18 @@ impl Snake{
         self.body.len()
     }
 
-    fn get_location(&self, body_index: usize) -> usize{
-        if body_index > self.len(){
+    fn get_location(&self, body_index: usize) -> usize {
+        if body_index > self.len() {
             return 0;
         }
 
         return self.body[body_index].0;
     }
 
-    fn change_direction(&mut self, direction: Direction){
-
+    fn change_direction(&mut self, direction: Direction) {
         let valid_move = match &self.direction_change {
             DirectionChange::Empty => Snake::direction_valid(&self.direction, &direction),
-            DirectionChange::Previous(from) => Snake::direction_valid(&from, &direction)
+            DirectionChange::Previous(from) => Snake::direction_valid(&from, &direction),
         };
 
         if !valid_move {
@@ -140,10 +183,11 @@ impl Snake{
     }
 
     fn direction_valid(from: &Direction, direction: &Direction) -> bool {
-        if *from == Direction::Left && *direction == Direction::Right ||
-           *from == Direction::Right && *direction == Direction::Left  ||
-           *from == Direction::Up && *direction == Direction::Down ||
-           *from == Direction::Down && *direction == Direction::Up  {
+        if *from == Direction::Left && *direction == Direction::Right
+            || *from == Direction::Right && *direction == Direction::Left
+            || *from == Direction::Up && *direction == Direction::Down
+            || *from == Direction::Down && *direction == Direction::Up
+        {
             return false;
         }
 
@@ -151,14 +195,14 @@ impl Snake{
     }
 
     fn grow(&mut self) {
-        let last_cell = self.body[ self.body.len() - 1].0;
+        let last_cell = self.body[self.body.len() - 1].0;
         self.body.push(Cell(last_cell));
     }
 
-    fn update(&mut self, width: usize, reward_cell: usize) -> SnakeUpdateState{
+    fn update(&mut self, width: usize, reward_cell: usize) -> SnakeUpdateState {
         let mut snake_update_state = SnakeUpdateState::Same;
 
-        if reward_cell == self.get_head(){
+        if reward_cell == self.get_head() {
             self.grow();
             snake_update_state = SnakeUpdateState::Grown;
         }
@@ -167,19 +211,26 @@ impl Snake{
             Direction::Right => self.move_right(width),
             Direction::Down => self.move_down(width),
             Direction::Left => self.move_left(width),
-            Direction::Up => self.move_up(width)
+            Direction::Up => self.move_up(width),
+        }
+
+        if self.is_dead() {
+            snake_update_state = SnakeUpdateState::Dead;
+        }
+
+        if self.is_won(width) {
+            snake_update_state = SnakeUpdateState::Won;
         }
 
         self.direction_change = DirectionChange::Empty;
         snake_update_state
     }
 
-    fn move_right(&mut self, width: usize){
+    fn move_right(&mut self, width: usize) {
         let mut index = self.get_head();
-        if (index + 1) % width == 0  {
+        if (index + 1) % width == 0 {
             index = index - (width - 1);
-        }
-        else{
+        } else {
             index += 1;
         }
 
@@ -187,7 +238,7 @@ impl Snake{
         self.body[0].0 = index;
     }
 
-    fn move_down(&mut self, width: usize){
+    fn move_down(&mut self, width: usize) {
         let mut index = self.get_head() + width;
 
         if index > width * width - 1 {
@@ -198,13 +249,12 @@ impl Snake{
         self.body[0].0 = index;
     }
 
-    fn move_left(&mut self, width: usize){
+    fn move_left(&mut self, width: usize) {
         let mut index = self.get_head();
 
-        if index % width == 0{
+        if index % width == 0 {
             index += width - 1;
-        }
-        else{
+        } else {
             index -= 1;
         }
 
@@ -212,13 +262,12 @@ impl Snake{
         self.body[0].0 = index;
     }
 
-    fn move_up(&mut self, width: usize){
+    fn move_up(&mut self, width: usize) {
         let mut index = self.get_head();
 
         if index < width {
-            index = width * width - width  + index
-        }
-        else{
+            index = width * width - width + index
+        } else {
             index -= width;
         }
 
@@ -226,19 +275,33 @@ impl Snake{
         self.body[0].0 = index;
     }
 
-    fn move_body(&mut self){
+    fn move_body(&mut self) {
         for i in (1..self.body.len()).rev() {
-            self.body[i].0 = self.body[i-1].0;
+            self.body[i].0 = self.body[i - 1].0;
         }
     }
 
-    fn index_taken(&self, index: usize) -> bool{
-        for i in &self.body{
-            if i.0 == index{
+    fn index_taken(&self, index: usize) -> bool {
+        for i in &self.body {
+            if i.0 == index {
                 return true;
             }
         }
 
         return false;
+    }
+
+    fn is_dead(&self) -> bool {
+        for i in 1..self.body.len() {
+            if self.body[0].0 == self.body[i].0 {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    fn is_won(&self, width: usize) -> bool {
+        self.body.len() == width * width - 1
     }
 }
--- www/index.ts
+++ www/index.ts
@@ -1,4 +1,4 @@
-import init, { World, Direction } from "snake_game/snake_game";
+import init, { World, Direction, GameStatus } from "snake_game/snake_game";
 
 init().then((_) => {
   const cellSize = 40;
@@ -6,108 +6,166 @@ init().then((_) => {
   const start_index = Date.now() % (width * width);
   const speed = 2;
 
-  const world = World.new(width, start_index);
-  const worldWidth = world.width();
+  const startBtn = document.getElementById("Start");
+  const failBtn = document.getElementById("Fail");
+  const wonBtn = document.getElementById("Won");
 
-  const canvas = <HTMLCanvasElement>document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
-
-  canvas.width = worldWidth * cellSize;
-  canvas.height = worldWidth * cellSize;
-
-  function drawWorld() {
-    ctx.beginPath();
-
-    for (let x = 0; x < worldWidth + 1; x++) {
-      ctx.moveTo(cellSize * x, 0);
-      ctx.lineTo(cellSize * x, cellSize * worldWidth);
-    }
-
-    for (let y = 0; y < worldWidth + 1; y++) {
-      ctx.moveTo(0, cellSize * y);
-      ctx.lineTo(cellSize * worldWidth, cellSize * y);
-    }
-
-    ctx.stroke();
+  function failed() {
+    failBtn.classList.remove("hide");
   }
 
-  function drawSnake() {
-    const len = world.snake_len();
-
-    for (let i = 0; i < len; i++) {
-      ctx.beginPath();
-      const position = world.snake_body_position(i);
-      const col = position % world.width();
-      ctx.fillStyle = i === 0 ? "white" : "black";
-      const row = Math.floor(position / world.width());
-      const radius = cellSize / 2;
-      ctx.arc(
-        col * cellSize + radius,
-        row * cellSize + radius,
-        radius,
-        0,
-        Math.PI * 2 * radius
-      );
-      ctx.fill();
-    }
+  function won() {
+    wonBtn.classList.remove("hide");
   }
 
-  function drawReward() {
-    const position = world.get_reward_cell();
-    const col = position % world.width();
-    const row = Math.floor(position / world.width());
-    ctx.beginPath();
-    ctx.fillStyle = "red";
-    ctx.fillRect(col * cellSize, row * cellSize, cellSize, cellSize);
-    ctx.fill();
-  }
+  function initGame(): () => void {
+    const world = World.new(width, start_index);
+    const worldWidth = world.width();
 
-  function update() {
-    setTimeout(() => {
-      ctx.clearRect(0, 0, world.width() * cellSize, world.width() * cellSize);
-      drawWorld();
-      drawReward();
-      drawSnake();
-      world.update();
-      requestAnimationFrame(update);
-    }, 500 / speed);
-  }
+    const canvas = <HTMLCanvasElement>document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
 
-  function change_directions() {
-    window.addEventListener("keydown", (e) => {
-      switch (e.code) {
-        case "ArrowUp":
-          change_direction(Direction.Up);
-          break;
-        case "KeyW":
-          change_direction(Direction.Up);
-          break;
-        case "ArrowDown":
-          change_direction(Direction.Down);
-          break;
-        case "KeyS":
-          change_direction(Direction.Down);
-          break;
-        case "ArrowLeft":
-          change_direction(Direction.Left);
-          break;
-        case "KeyA":
-          change_direction(Direction.Left);
-          break;
-        case "ArrowRight":
-          change_direction(Direction.Right);
-          break;
-        case "KeyD":
-          change_direction(Direction.Right);
-          break;
+    canvas.width = worldWidth * cellSize;
+    canvas.height = worldWidth * cellSize;
+
+    return function startGame() {
+      console.log("here 1");
+      console.log(world.get_game_status());
+      world.start_game();
+      console.log("here 2");
+
+      function drawWorld() {
+        ctx.beginPath();
+
+        for (let x = 0; x < worldWidth + 1; x++) {
+          ctx.moveTo(cellSize * x, 0);
+          ctx.lineTo(cellSize * x, cellSize * worldWidth);
+        }
+
+        for (let y = 0; y < worldWidth + 1; y++) {
+          ctx.moveTo(0, cellSize * y);
+          ctx.lineTo(cellSize * worldWidth, cellSize * y);
+        }
+
+        ctx.stroke();
       }
-    });
+
+      function drawSnake() {
+        const len = world.snake_len();
+
+        for (let i = 0; i < len; i++) {
+          ctx.beginPath();
+          const position = world.snake_body_position(i);
+          const col = position % world.width();
+          ctx.fillStyle = i === 0 ? "white" : "black";
+          const row = Math.floor(position / world.width());
+          const radius = cellSize / 2;
+          ctx.arc(
+            col * cellSize + radius,
+            row * cellSize + radius,
+            radius,
+            0,
+            Math.PI * 2 * radius
+          );
+          ctx.fill();
+        }
+      }
+
+      function drawReward() {
+        const position = world.get_reward_cell();
+        const col = position % world.width();
+        const row = Math.floor(position / world.width());
+        ctx.beginPath();
+        ctx.fillStyle = "red";
+        ctx.fillRect(col * cellSize, row * cellSize, cellSize, cellSize);
+        ctx.fill();
+      }
+
+      function update() {
+        const gameStatus = world.get_game_status();
+
+        switch (gameStatus) {
+          case 2:
+            won();
+            return;
+          case 3:
+            failed();
+            return;
+        }
+
+        setTimeout(() => {
+          ctx.clearRect(
+            0,
+            0,
+            world.width() * cellSize,
+            world.width() * cellSize
+          );
+          drawWorld();
+          drawReward();
+          drawSnake();
+          world.update();
+          requestAnimationFrame(update);
+        }, 500 / speed);
+      }
+
+      function change_directions() {
+        window.addEventListener("keydown", (e) => {
+          switch (e.code) {
+            case "ArrowUp":
+              change_direction(Direction.Up);
+              break;
+            case "KeyW":
+              change_direction(Direction.Up);
+              break;
+            case "ArrowDown":
+              change_direction(Direction.Down);
+              break;
+            case "KeyS":
+              change_direction(Direction.Down);
+              break;
+            case "ArrowLeft":
+              change_direction(Direction.Left);
+              break;
+            case "KeyA":
+              change_direction(Direction.Left);
+              break;
+            case "ArrowRight":
+              change_direction(Direction.Right);
+              break;
+            case "KeyD":
+              change_direction(Direction.Right);
+              break;
+          }
+        });
+      }
+
+      function change_direction(direction: Direction) {
+        world.change_direction(direction);
+      }
+
+      update();
+      change_directions();
+    };
   }
 
-  function change_direction(direction: Direction) {
-    world.change_direction(direction);
+  startBtn.addEventListener("click", onStart);
+  failBtn.addEventListener("click", onFail);
+  wonBtn.addEventListener("click", onWon);
+
+  function onStart(e: MouseEvent) {
+    startBtn.classList.add("hide");
+    initGame()();
   }
 
-  update();
-  change_directions();
+  function onFail(e: MouseEvent) {
+    failBtn.classList.add("hide");
+    startBtn.classList.remove("hide");
+    initGame();
+  }
+
+  function onWon(e: MouseEvent) {
+    wonBtn.classList.add("hide");
+    startBtn.classList.remove("hide");
+    initGame();
+  }
 });
